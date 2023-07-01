ce204b27c018662e507b4613dd521cae6f478b0f
mrworld196wide <shubhum19@gmail.com>
2023-07-02 02:18:59 +0530

    protected route created or home.jsx page

--- src/App.js
+++ src/App.js
@@ -11,14 +11,20 @@ function App() {
   // yeh user ki details fetch krlegs firebase se ki voh logged in hai ki nhi  
   const { currentUser } = useContext(AuthContext);
   console.log(currentUser);
-  
+  // jaise hi logout hoga we'll be redirected to login page
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children
+  };
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={ <Home /> } />
+            <Route index element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
           </Route>
