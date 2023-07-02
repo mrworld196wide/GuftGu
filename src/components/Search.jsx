d359431fefd89fb2efc17047ea9b7803e3e58e52
mrworld196wide <shubhum19@gmail.com>
2023-07-02 18:25:20 +0530

    searching user functionality added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
+    const [username, setUsername] = useState("");
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState(false);
+
+    // const { currentUser } = useContext(AuthContext);
+
+    const handleSearch = async() => {
+        //searching for the user in database
+        const q = query(
+            collection(db, "users"),
+            where("displayName", "==", username)
+        );
+
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data());
+            });
+        } catch (err) {
+            setErr(true);
+        }
+    };
+    const handleKey = (e) => {
+        e.code === 'Enter' && handleSearch()
+    };
     return (
         <div className='search'>
             <div className="searchForm">
-                <input type="text" placeholder='Find a user' />
+                {/* here ketdown listens to our keyboard actions */}
+                <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} value={username} />
             </div>
+            {/* error throw */}
+            {err && <span>User not found!</span>}
+            {/* user info */}
+            {user && <div className="userChat">
+                <img src={user.photoURL} alt="" />
+                <div className="userChatInfo">
+                    <span>{user.displayName}</span>
+                </div>
+            </div>}
         </div>
     )
 }
