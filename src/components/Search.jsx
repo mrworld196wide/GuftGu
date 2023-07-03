2d75e30beeb9a15c51e379ca6e727dfff7d6974f
mrworld196wide <shubhum19@gmail.com>
2023-07-03 18:25:32 +0530

    chat app basic database structure added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,9 +8,9 @@ const Search = () => {
     const [user, setUser] = useState(null);
     const [err, setErr] = useState(false);
 
-    // const { currentUser } = useContext(AuthContext);
-
-    const handleSearch = async() => {
+    const { currentUser } = useContext(AuthContext);
+    // handle searches
+    const handleSearch = async () => {
         //searching for the user in database
         const q = query(
             collection(db, "users"),
@@ -26,24 +26,62 @@ const Search = () => {
             setErr(true);
         }
     };
+    // yeh function enter press krte hi handleSearch function ko call karega
     const handleKey = (e) => {
         e.code === 'Enter' && handleSearch()
     };
+
+    // chat data
+    const handleSelect = async () => {
+        //check whether the group(chats in firestore) exists, if not create
+
+        // so combining user1 and user2 useId's
+        const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+        try {
+            const res = await getDoc(doc(db, "chats", combinedId));
+
+            // checking if the combined uid exist or not; if not creating one 
+            if (!res.exists()) {
+                //create a chat in chats collection
+                await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+                //create user chats
+                await updateDoc(doc(db, "userChats", currentUser.uid), {
+                    [combinedId + ".userInfo"]: {
+                        uid: user.uid,
+                        displayName: user.displayName,
+                        photoURL: user.photoURL,
+                    },
+                    // adding date at which message are send and received   to the database 
+                    [combinedId + ".date"]: serverTimestamp(),
+                });
+                await updateDoc(doc(db, "userChats", user.uid), {
+                    [combinedId + ".userInfo"]: {
+                        uid: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        photoURL: currentUser.photoURL,
+                    },
+                    [combinedId + ".date"]: serverTimestamp(),
+                });
+            }
+        } catch (err) {
+
+        }
+        setUser(null);
+        setUsername("")
+    };
     return (
-        <div className='search'>
+        <div className="search">
             <div className="searchForm">
-                {/* here ketdown listens to our keyboard actions */}
-                <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} value={username} />
+                <input type="text" placeholder="Find a user" onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} value={username} />
             </div>
-            {/* error throw */}
             {err && <span>User not found!</span>}
-            {/* user info */}
-            {user && <div className="userChat">
+            {user && (<div className="userChat" onClick={handleSelect}>
                 <img src={user.photoURL} alt="" />
                 <div className="userChatInfo">
                     <span>{user.displayName}</span>
                 </div>
-            </div>}
+            </div>)}
         </div>
     )
 }
