38b463bab4de9d87958aa38d442a3a5dd8d7272e
mrworld196wide <shubhum19@gmail.com>
2023-07-05 20:08:59 +0530

    fetch chat conversations

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 const Chats = () => {
+    const [chats, setChats] = useState([]);
+    const currentUser = useContext(AuthContext)
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+            return () => {
+                unsub();
+            };
+        };
+        currentUser.uid && getChats()
+    }, [currentUser.uid])
+    console.log(Object.entries(chats));
+
     return (
-        <div className='chats'>
-            <div className="userChat">
-                <img src="https://images.unsplash.com/photo-1509839862600-309617c3201e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-                <div className="userChatInfo">
-                    <span>Aishwar</span>
-                    {/* latest message */}
-                    <p>message</p>
+        <div className="chats">
+            {Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date).map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].lastMessage?.text}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="userChat">
-                <img src="https://images.unsplash.com/photo-1509839862600-309617c3201e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-                <div className="userChatInfo">
-                    <span>Aishwar</span>
-                    {/* latest message */}
-                    <p>message</p>
-                </div>
-            </div>
-            <div className="userChat">
-                <img src="https://images.unsplash.com/photo-1509839862600-309617c3201e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-                <div className="userChatInfo">
-                    <span>Aishwar</span>
-                    {/* latest message */}
-                    <p>message</p>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
